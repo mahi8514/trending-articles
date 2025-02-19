f7d6ba3263de6447d581d9c890507533bc24b07d
Mahin Ibrahim <mahin8514@gmail.com>
2025-02-19 23:28:48 +0400

    Initial commit

---
+++ trending-articles/trending_articlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  trending_articlesApp.swift
+//  trending-articles
+//
+//  Created by mahi  on 19/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct trending_articlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
